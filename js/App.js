8952e1417ef379afb87e479f4c17e46924ad6870
Caballero Edgardo <48573119+Caballeroedgar@users.noreply.github.com>
2022-05-21 19:18:56 -0300

    Add files via upload

---
+++ js/App.js
@@ -0,0 +1,7 @@
+
+
+$("header a i button ").on("click", function (e) {
+  e.preventDefault();
+  const href = $(this).attr("href");
+  $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
